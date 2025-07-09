d5e397a8c7e9d09c71561238c9b6343f03cd1fe2
swagxx <mi6499852@gmail.com>
2025-07-09 18:00:18 +0300

    Update nine.cpp

--- nine/nine.cpp
+++ nine/nine.cpp
@@ -1,3 +1,31 @@
-//
-// Created by SWAG on 28.06.2025.
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string text;
+    char symbol;
+
+    cout << "Введите строку: ";
+    getline(cin, text); 
+
+    cout << "Введите символ для поиска: ";
+    cin >> symbol; 
+
+    size_t first_pos = text.find(symbol);
+    size_t last_pos = text.rfind(symbol);
+
+    if (first_pos != string::npos) {
+        cout << "Первое вхождение символа '" << symbol << "' на позиции: "
+            << first_pos + 1 << endl; 
+        cout << "Последнее вхождение символа '" << symbol << "' на позиции: "
+            << last_pos + 1 << endl;
+    }
+    else {
+        cout << "Символ '" << symbol << "' не найден в строке." << endl;
+    }
+
+    return 0;
+}
